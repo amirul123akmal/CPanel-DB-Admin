a3e4be24d97b9552077a51041c5b613a3f726362
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2024-04-08 06:37:36 +0800

    Admin Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Table from './component/DatabaseTable.js'
+import Sidebar from './component/Sidebar.js'
+import Login from './component/Login.js';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'
+
+const darkTheme = createTheme({
+  palette:{
+    mode: "dark"
+  }
+})
 
 function App() {
+
+  const [switching, setSwitching] = useState('admin');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLoginFailure = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        {isLoggedIn ? (
+          <Sidebar type={switching}>
+            {/* <UserInterface /> */}
+            {switching === 'admin' ? <Table /> : "User Interface will replace here" }
+          </Sidebar>
+        ) : (
+          <Login onLoginSuccess={handleLoginSuccess} onLoginFailure={handleLoginFailure} />
+        )}
+      </ThemeProvider>
     </div>
   );
 }
